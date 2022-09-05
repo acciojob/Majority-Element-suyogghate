ecd03699fa7b1b79c66fe96d29d497e5f760f71d
Ghate Suyog <suyogghateminks@gmail.com>
2022-09-05 14:39:34 +0530

    completed

--- solution.js
+++ solution.js
@@ -2,5 +2,15 @@
 function major(arr)
 {
   //your code here
+   const threshold = Math.floor(arr.length / 2);
+
+   const map = {};
+   for (let i = 0; i < arr.length; i++) {
+      const value = arr[i];
+      map[value] = map[value] + 1 || 1;
+      if (map[value] > threshold)
+         return value;
+   };
+   return false;
 }
 module.exports=major;
